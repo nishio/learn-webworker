4d426fe638b39b5cd59cfba8807d0719571099a3
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-03-09 18:02:24 +0900

    try

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Worker from "./command.worker.ts";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const worker = new Worker();
+
+worker.postMessage({ a: 1 });
+worker.onmessage = function(event) {};
+
+worker.addEventListener("message", function(event) {});
